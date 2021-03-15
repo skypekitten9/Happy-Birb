369b9759eabbbd4999a272b701ac5b9a2566d117
skypekitten9 <victor.enhorning@gmail.com>
2021-03-15 16:55:59 +0100

    Added script to camera so it follows bird.

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public GameObject target;
+    float offSet;
+
+    private void Start()
+    {
+        offSet = transform.position.x;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x + offSet, transform.position.y, transform.position.z);
+    }
+}
