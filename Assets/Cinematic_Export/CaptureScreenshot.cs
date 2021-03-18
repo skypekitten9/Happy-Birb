23326cae2a882e1e13ffdeee99e3d941e77a898e
skypekitten9 <victor.enhorning@gmail.com>
2021-03-18 10:51:03 +0100

    Imported custom cinematic package for screen capture.

---
+++ Assets/Cinematic_Export/CaptureScreenshot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureScreenshot : MonoBehaviour
+{
+    public KeyCode keycode;
+    public string name;
+    public int quality;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keycode))
+        {
+            Debug.Log("Screen captured!");
+            ScreenCapture.CaptureScreenshot(name, quality);
+        }
+    }
+}
---
+++ Assets/Cinematic_Export/Follower.cs
@@ -0,0 +1,84 @@
+﻿using PathCreation;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public static bool showCinematic = true;
+
+    public static bool Following { get; set; } = false;
+
+    private PathCreator path;
+    [SerializeField] private float speed = 7.0f;
+    [SerializeField] private bool reversedPath = false;
+    [SerializeField] private bool reversedCamera = false;
+
+    private float t = 0;
+
+
+    private void Awake()
+    {
+        if (!showCinematic)
+        {
+            Following = false;
+            Destroy(transform.parent.gameObject);
+            return;
+        }
+        showCinematic = false;
+
+        Following = true;
+        path = transform.parent.GetComponentInChildren<PathCreator>();
+
+        if (reversedPath)
+        {
+            t = path.path.length;
+        }
+    }
+
+    private void Update()
+    {
+        if ((!reversedPath && t >= path.path.length) || (reversedPath && t <= 0))
+        {
+            Following = false;
+            Destroy(transform.parent.gameObject);
+        }
+
+        if (reversedPath)
+        {
+            t -= GetSpeed() * Time.deltaTime;
+        }
+        else
+        {
+            t += GetSpeed() * Time.deltaTime;
+        }
+
+        transform.position = path.path.GetPointAtDistance(t, EndOfPathInstruction.Stop);
+        Vector3 v = path.path.GetRotationAtDistance(t, EndOfPathInstruction.Stop).eulerAngles;
+
+        if (!reversedCamera)
+        {
+            transform.rotation = Quaternion.Euler(0, v.y, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, v.y + 180, 0);
+        }
+    }
+
+
+    float startStopDist = 10.0f;
+    private float GetSpeed()
+    {
+        if (t < startStopDist)
+        {
+            return Mathf.Abs(Mathf.Pow(t / startStopDist, 3)) * (speed - 1) + 1;
+        }
+        else if (t >= path.path.length - startStopDist)
+        {
+            return Mathf.Abs(Mathf.Pow((path.path.length - t) / startStopDist, 3)) * (speed - 1) + 1;
+        }
+        else
+        {
+            return speed;
+        }
+    }
+}
